f32f96ecdf5ba6d73e1c02e540185dcfa2963bd0
Omotayoopeyemi <solutionconsult111@gmail.com>
2022-10-18 08:14:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * print_alphabet_10x -print alphabet10 time
+ */
+void print_alphabet_x10(void)
+
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+	{
+for (c = 'a'; c <= 'z'; c++)
+{
+
+											_putchar(c);
+											}
+
+											_putchar('\n');
+
+												}
+
+}
