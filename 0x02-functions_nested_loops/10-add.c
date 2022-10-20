d876d389892ee004436e9dcc7fe9b35d59ec5928
Omotayoopeyemi <solutionconsult111@gmail.com>
2022-10-19 22:03:33 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * add - get the first value of two digit
+ * @n1: The first number
+ * @n2: The second number
+ * Return:  sum of (n1 and n2)
+ */
+int add(int i , int j);
+{
+return (i + j);
+}
