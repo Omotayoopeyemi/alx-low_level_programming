cacc2cc31bc6fcc55ad1be60befe14770da10db5
Omotayoopeyemi <solutionconsult111@gmail.com>
2022-10-20 20:31:36 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print_line - prints a line
+ *  @n: variable
+ *  Return: 0
+ */
+void print_line(int n)
+{
+int a = 0;
+while (a < n && n > 0)
+{
+putchar('_');
+a++;
+}
+putchar('\n');
+}
